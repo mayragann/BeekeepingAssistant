c4d5ddb763adaf000609de3804ade8005f21c610
mayragann <mayragann@gmail.com>
2022-05-19 11:25:48 -0400

    Added Inpsection Models

--- backend/inspection/models.py
+++ backend/inspection/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from hive.models import Hive
 
 # Create your models here.
+
+class Inspection(models.Model):
+     hive = models.ForeignKey(Hive, on_delete=models.CASCADE)
+     eggs = models.IntegerField()
+     larvae = models.IntegerField()
+     sealed_brood = models.IntegerField()
+     covered_bees = models.IntegerField()
+     nectar_honey = models.IntegerField()
+     pollen = models.IntegerField()
+     pest_spotted = models.CharField(max_length=200)
+     pest_action = models.CharField(max_length=200)
+     notes_concerns = models.CharField(max_length=300)
