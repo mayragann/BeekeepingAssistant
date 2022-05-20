6e18717444596b7fee7cba6a36fe5b380286ce94
mayragann <mayragann@gmail.com>
2022-05-20 12:23:25 -0400

    Completed Functionality for GET by inspection_id + GET by hive_id + POST new inspection to existing Hive + PUT existing inspection

--- backend/inspection/urls.py
+++ backend/inspection/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from inspection import views
 
 urlpatterns = [
-    # path('', views.update_inspection),
+    path('add/', views.new_inspection_by_hive),
      path('<int:pk>', views.inspection_details),
+     path('all/<int:pk>', views.get_inspection_by_hive)
 ]
--- backend/inspection/views.py
+++ backend/inspection/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from mysqlx import InsertStatement
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -8,34 +9,34 @@ from .serializers import InspectionSerializer
 from hive.models import Hive
 
 
-@api_view(['GET', 'POST','PUT'])
+@api_view(['GET','PUT'])
 @permission_classes([IsAuthenticated])
 def inspection_details(request, pk):
+    inspection = get_object_or_404(Inspection, pk=pk)
+    if request.method == "GET":
+        serializer = InspectionSerializer(inspection)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = InspectionSerializer(inspection, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET' ])
+@permission_classes([IsAuthenticated])
+
+def get_inspection_by_hive(request,pk):
     hive = get_object_or_404(Hive, pk=pk)
     inspection = Inspection.objects.filter(hive_id = hive.id, user=request.user)
     if request.method == "GET":
         serializer = InspectionSerializer(inspection, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
-    elif request.method == 'POST':
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def new_inspection_by_hive(request):
+    if request.method == 'POST':
         serializer = InspectionSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=request.user)
-            return Response(serializer.data,status.HTTP_200_OK)
+            return Response(serializer.data,status.HTTP_201_CREATED)
     
-    
-    elif request.method == 'PUT':
-        serializer = InspectionSerializer(hive, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(hive_id=request.hive.id)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-# @api_view(['PUT'])
-# @permission_classes([IsAuthenticated])
-# def update_inspection(request):
-#     hive = get_object_or_404(Hive)
-#     if request.method == 'PUT':
-#         serializer = InspectionSerializer(hive, data=request.data, many=True)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save(user=request.user)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
