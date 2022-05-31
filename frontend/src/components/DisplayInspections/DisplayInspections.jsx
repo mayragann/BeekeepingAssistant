82e67748b3fb9e65735faf46a02be0780d2c0798
mayragann <mayragann@gmail.com>
2022-05-31 11:44:41 -0400

    Added Styles to Display Inspection

--- frontend/src/components/DisplayInspections/DisplayInspections.jsx
+++ frontend/src/components/DisplayInspections/DisplayInspections.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Card from 'react-bootstrap/Card'
+import Inspection from './../../images/Inspection.svg'
+import "./DisplayInspections"
 
 import { useNavigate } from "react-router-dom";
 
@@ -19,31 +21,42 @@ const DisplayInspections = ({setSelectedInspection, inspections, setSelectedHive
     
     return ( 
         <>
-        
-            <table>
-                <tbody>
+
                     {inspections.map((inspection)=> {
                         return(
-                            <tr key={inspection.id} >
-                            <td>{inspection.eggs}</td>
-                            <td>{inspection.larvae} </td> 
-                            <td>{inspection.sealed_brood} </td> 
-                            <td>{inspection.covered_bees} </td> 
-                            <td>{inspection.nectar_honey} </td> 
-                            <td>{inspection.pollen} </td> 
-                            <td>{inspection.pest_spotted} </td> 
-                            <td>{inspection.pest_action} </td> 
-                            <td>{inspection.notes_concerns} </td> 
-                            <td>{inspection.inspection_date} </td> 
-                            
-                            <td><button onClick={() => handleClick(inspection)}>Update</button></td>
-                           
-                            
-                            </tr>
+                            <div key={inspection.id} className="card">
+                                 <Card style={{ width: '15rem' }} >
+                                 <Card.Img variant="top" src={Inspection} />
+                                 <Card.Body>
+                                {/* <Card.Title>Frames w/Eggs</Card.Title>
+                                <Card.Text>{inspection.eggs}</Card.Text>
+                                <Card.Title>Frames w/Larvae</Card.Title>
+                                <Card.Text>{inspection.larvae} </Card.Text> 
+                                <Card.Title>Frames w/Sealed Brood</Card.Title>
+                                <Card.Text>{inspection.sealed_brood} </Card.Text>
+                                <Card.Title>Frames Covered W/ Bees</Card.Title> 
+                                <Card.Text>{inspection.covered_bees} </Card.Text>
+                                <Card.Title>Frames w/Nectar</Card.Title> 
+                                <Card.Text>{inspection.nectar_honey} </Card.Text>
+                                <Card.Title>Frames w/Pollen</Card.Title> 
+                                <Card.Text>{inspection.pollen} </Card.Text>
+                                <Card.Title>Spotted Pests</Card.Title> 
+                                <Card.Text>{inspection.pest_spotted} </Card.Text> 
+                                <Card.Title>Pest Action</Card.Title>
+                                <Card.Text>{inspection.pest_action} </Card.Text> 
+                                <Card.Title>Notes or Concerns</Card.Title>
+                                <Card.Text>{inspection.notes_concerns} </Card.Text> */}
+                                <Card.Title>Inspection Date</Card.Title> 
+                                <Card.Text>{inspection.inspection_date} </Card.Text> 
+                                
+                                <button onClick={() => handleClick(inspection)}>Update</button>
+                                </Card.Body>
+                                </Card>
+                            </div>
                         )
                     })}
-                </tbody>
-            </table>
+                
+            
         </>
      );
 }
